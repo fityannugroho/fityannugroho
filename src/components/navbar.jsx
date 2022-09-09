c7fd095198424f4c372e83eef224a7eb6831be22
Fityan <fityannugroho@gmail.com>
2022-09-09 11:33:24 +0700

    Create `NavbarMenus`

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import {useRouter} from 'next/router';
 import {useEffect, useState} from 'react';
 import translation from '../utils/translation';
-import NavbarMenu from './navbar-menu';
+import NavbarMenus from './navbar-menus';
 import styles from './navbar.module.css';
 
 /**
@@ -50,16 +50,24 @@ export default function Navbar() {
         <Image src='/f-logo.ico' alt='Fit Logo' height='24' width='24' />
         <Link href='/'>fityannugroho</Link>
       </div>
-      <div id='toggleMenu' className={`${styles.toggle} ${isActive ? styles.cross : ''}`} onClick={onClickToggleMenu} onKeyUp={onKeyUpToggleMenu} tabIndex={0}>
+      <div id='toggleMenu'
+        className={`${styles.toggle} ${isActive ? styles.cross : ''}`}
+        onClick={onClickToggleMenu}
+        onKeyUp={onKeyUpToggleMenu}
+        tabIndex={0}
+      >
         <span className={styles.line1}></span>
         <span className={styles.line2}></span>
         <span className={styles.line3}></span>
       </div>
-      <ul className={`${styles.menus} ${isActive ? styles.active : '' }`}>
-        <NavbarMenu href='#about' text={t.get('navMenu1')} isParentVisible={isActive} />
-        <NavbarMenu href='#project' text={t.get('navMenu2')} isParentVisible={isActive} />
-        <NavbarMenu href='#contact' text={t.get('navMenu3')} isParentVisible={isActive} />
-      </ul>
+      <NavbarMenus
+        menus={[
+          {href: '#about', text: t.get('navMenu1')},
+          {href: '#project', text: t.get('navMenu2')},
+          {href: '#contact', text: t.get('navMenu3')},
+        ]}
+        isParentVisible={isActive}
+      />
     </nav>
   );
 }
