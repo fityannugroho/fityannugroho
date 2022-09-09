8ec2df07da349d369322c5d78426dc60370e82c6
Fityan <fityannugroho@gmail.com>
2022-09-09 11:15:17 +0700

    Create `NavbarMenu`

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import {useRouter} from 'next/router';
 import {useEffect, useState} from 'react';
 import translation from '../utils/translation';
+import NavbarMenu from './navbar-menu';
 import styles from './navbar.module.css';
 
 /**
@@ -33,22 +34,32 @@ export default function Navbar() {
     window.addEventListener('resize', setNavbarBackground);
   }, []);
 
+  const onClickToggleMenu = () => {
+    setActive(!isActive);
+  };
+
+  const onKeyUpToggleMenu = (event) => {
+    if (event.key === 'Enter') {
+      setActive(!isActive);
+    }
+  };
+
   return (
     <nav className={`${styles.navbar} ${isTransparent ? styles.transparent : ''}`}>
       <div className={styles.logo}>
         <Image src='/f-logo.ico' alt='Fit Logo' height='24' width='24' />
         <Link href='/'>fityannugroho</Link>
       </div>
-      <ul className={`${styles.menus} ${isActive ? styles.active : '' }`}>
-        <li className={styles.menu}><Link href='#skills'>{t.get('navMenu1')}</Link></li>
-        <li className={styles.menu}><Link href='#project'>{t.get('navMenu2')}</Link></li>
-        <li className={styles.menu}><Link href='#contact'>{t.get('navMenu3')}</Link></li>
-      </ul>
-      <div id='toggleMenu' className={`${styles.toggle} ${isActive ? styles.cross : ''}`} onClick={() => setActive(!isActive)}>
+      <div id='toggleMenu' className={`${styles.toggle} ${isActive ? styles.cross : ''}`} onClick={onClickToggleMenu} onKeyUp={onKeyUpToggleMenu} tabIndex={0}>
         <span className={styles.line1}></span>
         <span className={styles.line2}></span>
         <span className={styles.line3}></span>
       </div>
+      <ul className={`${styles.menus} ${isActive ? styles.active : '' }`}>
+        <NavbarMenu href='#about' text={t.get('navMenu1')} isParentVisible={isActive} />
+        <NavbarMenu href='#project' text={t.get('navMenu2')} isParentVisible={isActive} />
+        <NavbarMenu href='#contact' text={t.get('navMenu3')} isParentVisible={isActive} />
+      </ul>
     </nav>
   );
 }
