95e78aa16fc892ea611225ad8f55e35d4ab645a8
Fityan <fityannugroho@gmail.com>
2022-09-13 10:30:32 +0700

    Fix double-quotes usage in page-head.jsx

--- src/components/page-head.jsx
+++ src/components/page-head.jsx
@@ -15,13 +15,13 @@ export default function PageHead({
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content={description} />
-        <meta name="keywords" content={keywords} />
-        <link rel="shortcut icon" href="/f-logo.ico" type="image/x-icon" />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name='description' content={description} />
+        <meta name='keywords' content={keywords} />
+        <link rel='shortcut icon' href='/f-logo.ico' type='image/x-icon' />
         <title>{title}</title>
       </Head>
-      <Script src="https://kit.fontawesome.com/a4efd995d2.js" crossOrigin='anonymous' />
+      <Script src='https://kit.fontawesome.com/a4efd995d2.js' crossOrigin='anonymous' />
     </>
   );
 }
