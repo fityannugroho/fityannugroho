2843709521886cefd2b2eecda0f804c15fc072bf
Fityan <fityannugroho@gmail.com>
2023-03-21 07:20:47 +0700

    fix(parallax): translate the parallax content

--- src/components/parallax.jsx
+++ src/components/parallax.jsx
@@ -1,3 +1,5 @@
+import {useRouter} from 'next/router';
+import translation from '../utils/translation';
 import styles from './parallax.module.css';
 
 /**
@@ -5,11 +7,14 @@ import styles from './parallax.module.css';
  * @return {JSX.Element}
  */
 export default function Parallax() {
+  const {locale, defaultLocale} = useRouter();
+  const t = translation(locale ?? defaultLocale);
+
   return (
     <div className={styles.parallax}>
       <div className={styles.inner}>
-        <h1>Fityandhiya Islam Nugroho</h1>
-        <h2 lang='en'>Software Engineer</h2>
+        <h1>{t.get('parallaxTitle')}</h1>
+        <h2>{t.get('parallaxSubtitle')}</h2>
       </div>
     </div>
   );
