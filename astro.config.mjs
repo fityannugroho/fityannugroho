8188440f154ccebe461f3c67738dbb11e8e919c6
Fityan <fityannugroho@gmail.com>
2024-01-20 15:51:33 +0700

    install shadcn/ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,21 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+// https://vercel.com/docs/projects/environment-variables/system-environment-variables#system-environment-variables
+const VERCEL_PREVIEW_SITE =
+  process.env.VERCEL_ENV !== "production" &&
+  process.env.VERCEL_URL &&
+  `https://${process.env.VERCEL_URL}`;
+
+const site = VERCEL_PREVIEW_SITE || "https://fityannugroho.vercel.app";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site,
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
 });
