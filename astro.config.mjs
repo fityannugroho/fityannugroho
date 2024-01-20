0cd557b1386d09452bfde90db034516e030b9cef
Fityan <fityannugroho@gmail.com>
2024-01-20 11:27:05 +0700

    add tailwind and react integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,8 @@
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-export default defineConfig({});
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react()],
+});
