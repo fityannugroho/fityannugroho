d0bac487378b04b6186ccda63ac6ad1eba7b759c
Fityan <fityannugroho@gmail.com>
2024-01-31 13:55:39 +0700

    improve web assets

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,7 +30,7 @@ export function Navbar({ items, current }: NavbarProps) {
         {/* Logo */}
         <div className="flex items-center gap-4 order-first">
           <a href="/" className="flex items-center gap-1">
-            <img src="/f-logo.ico" alt="Logo" className="w-6 h-6" />
+            <img src="/favicon.svg" alt="Logo" className="h-6" />
             <span className="text-primary md:inline-block hidden">
               fityannugroho
             </span>
