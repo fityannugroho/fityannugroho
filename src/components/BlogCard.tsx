bf8d51ff0f0aea0c555a8ac5fc948d077d19700e
Fityan <fityannugroho@gmail.com>
2024-02-02 11:04:47 +0700

    improve blog page, add min-height for main

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -19,11 +19,21 @@ export function BlogCard({ data }: BlogCardProps) {
   return (
     <Card>
       <CardHeader>
+        {data.image && (
+          <img
+            src={data.image.src}
+            alt={data.image.alt}
+            title={data.image.title}
+            className="rounded-sm mb-4 h-40 w-full object-cover"
+            height={160}
+            width={320}
+          />
+        )}
         <CardTitle>{data.title}</CardTitle>
         <CardDescription>{data.summary}</CardDescription>
       </CardHeader>
       <CardFooter>
-        <div className="flex gap-1">
+        <div className="flex gap-2">
           {data.tags.map((tag) => (
             <Badge key={tag}>{tag}</Badge>
           ))}
