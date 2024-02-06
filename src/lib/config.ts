3ffdafa2d42b76b95b2c6aae40fd17e0b9635b80
Fityan <fityannugroho@gmail.com>
2024-02-06 21:46:00 +0700

    feat: improve title, description, og image (#214)

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,16 @@
 export default {
+  /**
+   * The default title for the website
+   */
+  title: "fityannugroho | Software Engineer",
+  /**
+   * The default description for the website
+   */
+  description:
+    "A passionate software engineer from Indonesia who is proficient in PHP and TypeScript. Leverages various frameworks and tools to build robust web applications.",
+  /**
+   * The default image for the website
+   */
+  ogImage: "/og-image.png",
   githubToken: import.meta.env.GITHUB_TOKEN as string | undefined,
 } as const;
