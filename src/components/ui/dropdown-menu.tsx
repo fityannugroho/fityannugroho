ac00142e9620ec2270f1dbf86f65b203f8356cf0
Fityan <63894003+fityannugroho@users.noreply.github.com>
2024-11-26 14:24:46 +0700

    style: fix

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -1,9 +1,5 @@
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
-import {
-  CheckIcon,
-  ChevronRightIcon,
-  DotIcon,
-} from "lucide-react";
+import { CheckIcon, ChevronRightIcon, DotIcon } from "lucide-react";
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
