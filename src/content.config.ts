8805d3189b081e031d2a331f32a38884a1f048e2
Fityan <63894003+fityannugroho@users.noreply.github.com>
2025-04-13 21:19:12 +0700

    feat: render projects data from the CMS (#261)

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
 import { type SchemaContext, defineCollection, z } from "astro:content";
-import { file, glob } from "astro/loaders";
+import { glob } from "astro/loaders";
 
 // 3. Define your collection(s)
 export const blogSchema = (ctx: SchemaContext) =>
@@ -22,20 +22,5 @@ const blog = defineCollection({
   schema: (ctx) => blogSchema(ctx),
 });
 
-const projectSchema = z.object({
-  name: z.string(),
-  description: z.string().optional(),
-  links: z.object({
-    site: z.string().optional(),
-    download: z.string().optional(),
-    code: z.string().optional(),
-  }),
-});
-
-const projects = defineCollection({
-  loader: file("./src/data/projects.json"),
-  schema: projectSchema,
-});
-
 // 4. Export a single `collections` object to register your collection(s)
-export const collections = { blog, projects };
+export const collections = { blog };
